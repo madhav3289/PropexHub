16a3820be6b1aab23ab06cda5d3cd8512347ca7f
madhav3289 <madhavguptadavfps1234@gmail.com>
2025-12-24 00:31:05 +0530

    Updated files and ensured .env is ignored

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://madhav3289:Lt3a4jFXqrt3289@mern-estate.ibdcdcv.mongodb.net/?appName=mern-estate").then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+});
 
 const app=express();
 
