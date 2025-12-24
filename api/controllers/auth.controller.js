0321fca48056295da07cbf2803ab33804f63d0a7
madhav3289 <madhavguptadavfps1234@gmail.com>
2025-12-24 22:09:34 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,28 +1,37 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup=async (req,res,next) => {
-    const {username,email,password}=req.body;
-    try {
-        // Check if user already exists by username or email
-        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
-        if (existingUser) {
-            if (existingUser.username === username) {
-                return res.status(409).json({ success: false, message: 'Username is already taken' });
-            }
-            if (existingUser.email === email) {
-                return res.status(409).json({ success: false, message: 'Email is already registered' });
-            }
-        }
-
-        // If no existing user, create new user
-        const hashedPassword = bcrypt.hashSync(password, 10);
-        const newUser = new User({ username, email, password: hashedPassword });
-        await newUser.save();
-        res.status(201).json({ success: true, message: 'User created successfully!' });
-    } catch (error) {
-        // Handle other potential errors (e.g., database connection issues)
-        res.status(500).json({ success: false, message: error.message });
-    }
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json('User created successfully!');
+  } catch (error) {
+    next(error);
+  }
 };
+
+
+export const signin=async (req,res,next) => {
+    const {email,password}=req.body;
+    try{
+        const validUser=await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,'User not found!'));
+        }
+        const validPassword=bcrypt.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401,'Wrong credentials'));
+        }
+        const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const {password:pass, ...rest}=validUser._doc;
+        res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
+    }
+    catch(error){
+        next(error);
+    }
+}
